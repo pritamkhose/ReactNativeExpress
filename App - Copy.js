e94a247a032d22e1f9fc43dfecf3ad181806a8bf
pritam <pritamkhose@gmail.com>
2020-03-01 18:09:37 +0530

    expo 36 update

--- App - Copy.js
+++ App - Copy.js
@@ -1,35 +1,19 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-// You can import from local files
-import AssetExample from './components/AssetExample';
-import CounterExample from './components/CounterExample';
-import ViewExample from './components/ViewExample';
-import Flexbox from './components/Flexbox';
-import TextImage from './components/TextImage';
-import ScrollviewExample from './components/ScrollviewExample';
-import FlatListExample from './components/FlatListExample';
-import SectionListExample from './components/SectionListExample';
-
-import DataCompExample from './data_component/DataCompExample';
-import NetworkingExample from './networking/NetworkingExample';
-import IndexReduxExample from './networking/IndexReduxExample';
-
-import AnimatedExample from './animation/AnimatedExample';
-import RNAnimatedExample from './animation/RNAnimatedExample';
-import GesturesExample from './animation/GesturesExample';
-
-import AsyncStorageExample from './components/AsyncStorageExample';
-
-// http://www.reactnativeexpress.com/
-export default class App extends React.Component {
-  render() {
-    // <AssetExample />   <CounterExample /> <ViewExample />  <Flexbox /> <TextImage />  <ScrollviewExample />  <FlatListExample />   <SectionListExample /> <DataCompExample />  <NetworkingExample />  <IndexReduxExample /> <AnimatedExample /> <RNAnimatedExample /> <GesturesExample /> <AsyncStorageExample />
-
-    return <CounterExample />;
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
